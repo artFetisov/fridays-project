6e7b2f3097b3070fc2bac852be048e7102082116
artFetisov <fetishfestoff@mail.ru>
2023-01-18 23:48:43 +0300

    refactor

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,25 +1,36 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps, FC} from 'react'
-import styles from './Button.module.scss'
+import React, { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
+
 import cn from 'classnames'
 
-type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+import styles from './Button.module.scss'
+
+type DefaultButtonPropsType = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>
 
 type ButtonPropsType = DefaultButtonPropsType & {
-    big?: boolean
-    variant?: 'white' | 'blue' | 'no-radius' | 'red'
+  big?: boolean
+  variant?: 'white' | 'blue' | 'no-radius' | 'red'
 }
 
-export const Button: FC<ButtonPropsType> =
-    ({big, className, variant, disabled, ...restProps}) => {
-        return <button
-            className={cn(styles.default, {
-                [styles.big]: big,
-                [styles.white]: variant === 'white',
-                [styles.red]: variant === 'red',
-                [styles.noRadius]: variant === 'no-radius',
-                [styles.disabled]: disabled
-            })}
-            {...restProps}
-        />
-    }
-
+export const Button: FC<ButtonPropsType> = ({
+  big,
+  className,
+  variant,
+  disabled,
+  ...restProps
+}) => {
+  return (
+    <button
+      className={cn(styles.default, {
+        [styles.big]: big,
+        [styles.white]: variant === 'white',
+        [styles.red]: variant === 'red',
+        [styles.noRadius]: variant === 'no-radius',
+        [styles.disabled]: disabled,
+      })}
+      {...restProps}
+    />
+  )
+}
